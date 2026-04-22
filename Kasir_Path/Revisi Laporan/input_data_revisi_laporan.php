951904baa9fac2b9275b8ed1e258f6c04cb52c51
Kqurisna <king.kzxuan246@gmail.com>
2026-04-22 19:13:30 +0700

    Menu Kasir Toko = Revisi Laporan [Fix] -> [Fix celah saat revisi data] + [delete data]

--- Kasir_Path/Revisi Laporan/input_data_revisi_laporan.php
+++ Kasir_Path/Revisi Laporan/input_data_revisi_laporan.php
@@ -1,131 +1,225 @@
 <?php
+ob_start();
+date_default_timezone_set('Asia/Jakarta');
+
 $koneksi = mysqli_connect("localhost", "root", "", "pt_bumijaya");
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
 
 $id_nota = $_POST['id_nota'];
 
-$nomor = $_POST['nomer_nota'];
-$tanggal = $_POST['tanggal_nota'];
-$supplier = $_POST['supplier'];
-$jenis = $_POST['jenis_barang'];
+$updateFields = [];
 
-$fotoNotaBaru = $_FILES['foto_nota']['name'];
-$fotoNotaLama = $_POST['foto_nota_lama'];
+$nomor   = $_POST['nomer_nota'] ?? '';
+$tanggal = $_POST['tanggal_nota'] ?? '';
+$supplier = $_POST['supplier'] ?? '';
+$jenis   = $_POST['jenis_barang'] ?? '';
 
-if (!empty($fotoNotaBaru)) {
+if (!empty($nomor)) {
+    $updateFields[] = "nomor_nota = '$nomor'";
+}
+
+if (!empty($tanggal)) {
+    $updateFields[] = "tanggal_nota = '$tanggal'";
+}
+
+if (!empty($supplier)) {
+    $updateFields[] = "supplier = '$supplier'";
+}
+
+if (!empty($jenis)) {
+    $updateFields[] = "jenis_barang = '$jenis'";
+}
+
+$pathFolder = "../../AdminGudang_Path/Input Nota Barang Masuk/";
+
+$fotoNotaBaru = $_FILES['foto_nota']['name'] ?? '';
+$fotoNotaLama = $_POST['foto_nota_lama'] ?? '';
+
+if (isset($_FILES['foto_nota']) && $_FILES['foto_nota']['error'] === 0) {
+
     $tmp = $_FILES['foto_nota']['tmp_name'];
-    $namaFile = time() . "_" . $fotoNotaBaru;
+    $namaFile = time() . "_" . rand(1000, 9999) . "_" . $fotoNotaBaru;
 
-    move_uploaded_file($tmp, "../../AdminGudang_Path/Input Nota Barang Masuk/" . $namaFile);
+    $pathBaru = $pathFolder . $namaFile;
+    $pathLama = $pathFolder . $fotoNotaLama;
+
+    move_uploaded_file($tmp, $pathBaru);
+
+    if (!empty($fotoNotaLama) && $fotoNotaLama !== $namaFile && file_exists($pathLama)) {
+        unlink($pathLama);
+    }
 
     $fotoNota = $namaFile;
 } else {
     $fotoNota = $fotoNotaLama;
 }
 
-mysqli_query($koneksi, "
-    UPDATE nota SET 
-        nomor_nota = '$nomor',
-        tanggal_nota = '$tanggal',
-        supplier = '$supplier',
-        jenis_barang = '$jenis',
-        foto_nota = '$fotoNota'
-    WHERE id_nota = $id_nota
-");
+$updateFields[] = "foto_nota = '$fotoNota'";
 
-$barang = $_POST['barang'];
-$jumlah = $_POST['jumlah'];
+$q1 = true;
 
-$queryDetail = mysqli_query($koneksi, "SELECT * FROM detail_barang WHERE id_nota = $id_nota");
+if (!empty($updateFields)) {
+    $setQuery = implode(", ", $updateFields);
 
-$i = 0;
-while ($d = mysqli_fetch_assoc($queryDetail)) {
-
-    $id_detail = $d['id_detail'];
-
-    mysqli_query($koneksi, "
-        UPDATE detail_barang SET
-            nama_barang = '" . $barang[$i] . "',
-            jumlah_barang = '" . $jumlah[$i] . "'
-        WHERE id_detail = $id_detail
+    $q1 = mysqli_query($koneksi, "
+        UPDATE nota SET $setQuery
+        WHERE id_nota = $id_nota
     ");
-
-    $i++;
 }
 
+$q2 = true;
+
+if (isset($_POST['barang'])) {
+
+    $barang = $_POST['barang'];
+    $jumlah = $_POST['jumlah'];
+
+    $queryDetail = mysqli_query($koneksi, "SELECT * FROM detail_barang WHERE id_nota = $id_nota");
+
+    $i = 0;
+
+    while ($d = mysqli_fetch_assoc($queryDetail)) {
+
+        $id_detail = $d['id_detail'];
+        $fields = [];
+
+        if (!empty($barang[$i])) {
+            $fields[] = "nama_barang = '" . $barang[$i] . "'";
+        }
+
+        if ($jumlah[$i] !== "") {
+            $fields[] = "jumlah_barang = '" . $jumlah[$i] . "'";
+        }
+
+        if (!empty($fields)) {
+            $set = implode(", ", $fields);
+
+            if (!mysqli_query($koneksi, "
+                UPDATE detail_barang SET $set
+                WHERE id_detail = $id_detail
+            ")) {
+                $q2 = false;
+            }
+        }
+
+        $i++;
+    }
+}
+
+$q3 = true;
 
 if (isset($_POST['keterangan'])) {
     foreach ($_POST['keterangan'] as $id_detail => $ket) {
 
+        $fields = [];
+
+        if (!empty($ket)) {
+            $fields[] = "keterangan = '$ket'";
+        }
+
         $fotoLama = $_POST['foto_bukti_lama'][$id_detail] ?? '';
         $fotoBaru = $_FILES['foto_retur']['name'][$id_detail] ?? '';
 
-        if (!empty($fotoBaru)) {
+        if (!empty($fotoBaru) && $_FILES['foto_retur']['error'][$id_detail] === 0) {
 
             $tmp = $_FILES['foto_retur']['tmp_name'][$id_detail];
-            $namaFile = time() . "_" . $fotoBaru;
+            $namaFile = time() . "_" . rand(1000, 9999) . "_" . $fotoBaru;
 
             move_uploaded_file($tmp, "../../Kasir_Path/Pengecekkan Barang FIsik/uploads/bukti/" . $namaFile);
 
-            $fotoFinal = $namaFile;
-        } else {
-            $fotoFinal = $fotoLama;
+            // hapus foto lama
+            $pathLama = "../../Kasir_Path/Pengecekkan Barang FIsik/uploads/bukti/" . $fotoLama;
+            if (!empty($fotoLama) && $fotoLama !== $namaFile && file_exists($pathLama)) {
+                unlink($pathLama);
+            }
+
+            $fields[] = "foto_bukti = '$namaFile'";
+        } else if (!empty($fotoLama)) {
+            $fields[] = "foto_bukti = '$fotoLama'";
         }
 
-        mysqli_query($koneksi, "
-            UPDATE validasi_kasir SET
-                keterangan = '$ket',
-                foto_bukti = '$fotoFinal'
-            WHERE id_detail = $id_detail AND id_nota = $id_nota
-        ");
+        if (!empty($fields)) {
+            $set = implode(", ", $fields);
+
+            if (!mysqli_query($koneksi, "
+                UPDATE validasi_kasir SET $set
+                WHERE id_detail = $id_detail AND id_nota = $id_nota
+            ")) {
+                $q3 = false;
+            }
+        }
     }
 }
 
+$q4 = true;
 
 if (isset($_POST['jumlah_retur'])) {
     foreach ($_POST['jumlah_retur'] as $id_detail => $jml) {
 
-        mysqli_query($koneksi, "
-            UPDATE retur_detail SET
-                jumlah_retur = '$jml'
-            WHERE id_detail = $id_detail
-        ");
+        if ($jml !== "") {
+            if (!mysqli_query($koneksi, "
+                UPDATE retur_detail SET
+                    jumlah_retur = '$jml'
+                WHERE id_detail = $id_detail
+            ")) {
+                $q4 = false;
+            }
+        }
     }
 }
 
-$fotoSupplierBaru = $_FILES['foto_supplier']['name'];
-$fotoSupplierLama = $_POST['lampiran_lama'];
+$q5 = true;
 
-if (!empty($fotoSupplierBaru)) {
+$fotoSupplierBaru = $_FILES['foto_supplier']['name'] ?? '';
+$fotoSupplierLama = $_POST['lampiran_lama'] ?? '';
+
+$lampiran = '';
+
+if (!empty($fotoSupplierBaru) && $_FILES['foto_supplier']['error'] === 0) {
 
     $tmp = $_FILES['foto_supplier']['tmp_name'];
-    $namaFile = time() . "_" . $fotoSupplierBaru;
+    $namaFile = time() . "_" . rand(1000, 9999) . "_" . $fotoSupplierBaru;
 
     move_uploaded_file($tmp, "../../AdminGudang_Path/Input Konfirmasi Retur Supplier/uploads/tanggapan_supplier/" . $namaFile);
 
-    $fotoSupplier = $namaFile;
+    // hapus lama
+    $pathLama = "../../AdminGudang_Path/Input Konfirmasi Retur Supplier/uploads/tanggapan_supplier/" . $fotoSupplierLama;
+    if (!empty($fotoSupplierLama) && $fotoSupplierLama !== $namaFile && file_exists($pathLama)) {
+        unlink($pathLama);
+    }
+
+    $lampiran = $namaFile;
 } else {
-    $fotoSupplier = $fotoSupplierLama;
+    $lampiran = $fotoSupplierLama;
 }
 
 $qRetur = mysqli_query($koneksi, "SELECT id_retur FROM retur WHERE id_nota = $id_nota");
 $r = mysqli_fetch_assoc($qRetur);
 $id_retur = $r['id_retur'] ?? 0;
 
-if ($id_retur) {
-    mysqli_query($koneksi, "
+if ($id_retur && !empty($lampiran)) {
+    if (!mysqli_query($koneksi, "
         UPDATE tanggapan_supplier SET
-            lampiran = '$fotoSupplier'
+            lampiran = '$lampiran'
         WHERE id_retur = $id_retur
-    ");
+    ")) {
+        $q5 = false;
+    }
 }
 
-mysqli_query($koneksi, "
+$q6 = mysqli_query($koneksi, "
     UPDATE nota 
     SET status_laporan = 'menunggu'
     WHERE id_nota = $id_nota
 ");
 
-echo "<script>
-    alert('Data revisi berhasil disimpan!');
-    window.location.href = 'list_nota_ditolak.php';
-</script>";
+if ($q1 && $q2 && $q3 && $q4 && $q5 && $q6) {
+
+    header("Location: input_revisi_laporan_berhasil.php");
+    exit;
+} else {
+    echo "Terjadi kesalahan saat menyimpan data.";
+}
