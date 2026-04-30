9aaa57dddb2394daf75bdda39ee727ae886f5a4b
Kqurisna <king.kzxuan246@gmail.com>
2026-04-30 10:28:04 +0700

    Menu Kasir Toko = Pengecekkan Barang Fisik [Edit] -> [Memperbaiki title agar sesuai dengan konteks halaman]

--- Kasir_Path/Pengecekkan Barang FIsik/list_nota.php
+++ Kasir_Path/Pengecekkan Barang FIsik/list_nota.php
@@ -16,7 +16,7 @@ $query = mysqli_query($koneksi, "
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Input Nota Barang Masuk</title>
+    <title>Pengecekkan Barang Fisik</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
 
